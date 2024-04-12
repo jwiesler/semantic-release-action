ea0fcc705aeb44f59fb5a5ff7fea9d827493115f
Julian Wiesler <wiesleju@gmail.com>
2024-04-12 13:16:04 +0200

    Test

--- src/preInstall.task.js
+++ src/preInstall.task.js
@@ -14,6 +14,7 @@ module.exports = async extras => {
   const _extras = extras.replace(/['"]/g, '').replace(/[\n\r]/g, ' ');
   const silentFlag = process.env.RUNNER_DEBUG === '1' ? '' : '--silent';
 
+  core.info(`npm install ${_extras} --no-audit ${silentFlag}`);
   const { stdout, stderr } = await exec(`npm install ${_extras} --no-audit ${silentFlag}`, {
     cwd: path.resolve(__dirname, '..')
   });
